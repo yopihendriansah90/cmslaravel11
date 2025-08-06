fd21af8f7b25278b41c2e32583b189d9f2569e59
Yopi Hendriansah <yopihendriansah90@gmail.com>
2025-08-06 15:19:20 +0700

    baru sampai penjelasan migration di laravel 11

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Route::get('/blog',function(){
+
+//     $profile = 'aku programme3r';
+//  return view('blog',['data'=>$profile]);
+// })->name('blog');
+
+
+Route::get('/blog',[BlogController::class,'index']);
+
+// Route::get('/blog/{id}',function(Request $request){
+//     return redirect()->route('blog');
+// });
